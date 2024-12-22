2afa9a501059c62b36a9c8b841adda3fb20dda43
FaizAP412 <faiz.pribadi@gmail.com>
2024-12-22 13:33:44 +0700

    merge

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:bite_tracker_mobile/feature/authentication/pages/login.dart';
-import 'package:bite_tracker_mobile/feature/main/pages/menu.dart';
-import 'package:bite_tracker_mobile/feature/sharebites/screens/sharebites_screen.dart';
 import 'package:flutter/material.dart';
-import "package:bite_tracker_mobile/feature/mybites/screens/menu.dart";
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 
